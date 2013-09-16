b02433cab654f78f78855fc4c111c4d2a82e9365
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2013-09-16 14:48:18 +0200

    attoparsec version of tex-check

---
+++ extras/tex-check-attoparsec.hs
@@ -0,0 +1,287 @@
+{-# LANGUAGE OverloadedStrings, NamedFieldPuns, DeriveDataTypeable #-}
+module Main where
+
+import Prelude hiding (takeWhile) -- TODO
+
+import Data.Char
+import Data.Attoparsec.ByteString.Char8 hiding (isEndOfLine)
+import Data.ByteString.Char8 (ByteString, unpack)
+import qualified Data.ByteString.Char8 as ByteString
+import Data.Typeable
+import Data.Monoid
+
+import Control.Applicative
+import Control.Exception
+import Control.Monad
+
+import System.Environment
+import System.Exit
+
+import Output
+
+-- Allereerst definiëren we een paar type-synoniemen. We gebruiken een
+-- eenvoudige lijst als stapel (Stack), een foutmelding is een string en een
+-- regelnummer is een geheel getal.
+type Stack a = [a]
+
+-- | Een symbool is een abstract data type. Deze kunnen we goedkoper op de
+-- stapel zetten dan strings. Een kleine optimalisatie dus. We leiden een
+-- instantie voor @Eq@ en @Ord@ af en maken er zelf eentje voor @Show@ aan.
+data Symbol  = Brace
+             | Bracket
+             | Paren
+             | Angle
+             | Dollar
+             | At
+             | Delimiter
+             | StartStop Name
+             | BeginEnd  Name
+             deriving (Show, Eq, Ord)
+type Name    = ByteString
+
+openOf :: Symbol -> ByteString
+openOf s = case s of
+  Brace       -> "{"
+  Bracket     -> "["
+  Paren       -> "("
+  Angle       -> "<"
+  Dollar      -> "$"
+  At          -> "@"
+  Delimiter   -> "\\left"
+  StartStop n -> "\\start" <> n
+  BeginEnd  n -> "\\begin{" <> n <> "}"
+
+closeOf :: Symbol -> ByteString
+closeOf s = case s of
+  Brace       -> "}"
+  Bracket     -> "]"
+  Paren       -> ")"
+  Angle       -> ">"
+  Dollar      -> "$"
+  At          -> "@"
+  Delimiter   -> "\\right"
+  StartStop n -> "\\stop" <> n
+  BeginEnd  n -> "\\end{" <> n <> "}"
+
+type Line    = Int
+data Mode  = Normal | Math
+           deriving (Show, Eq, Ord, Enum)
+data State = State { mode  :: Mode
+                   , line  :: Line
+                   , stack :: Stack (Line,Symbol)
+                   } deriving (Show, Eq)
+
+mkState :: State
+mkState = State { mode  = Normal
+                , line  = 1
+                , stack = [] }
+
+data BalancingError = EndOfFile Symbol Line
+                    | DoesNotMatch Symbol Line Symbol Line
+                    | ClosedWithoutOpening Symbol Line
+                    | Unknown String
+                    deriving (Typeable)
+instance Exception BalancingError
+
+instance Show BalancingError where
+  show e = case e of
+    EndOfFile s' l'          ->                            "Unexpected end of file, expected '"                    ++ unpack (closeOf s') ++ "'\n   " ++ "(to close '" ++ unpack (openOf s') ++ "' from line " ++ show l' ++ ")"
+    DoesNotMatch s l s' l'   -> "Line " ++ show l ++ ":\n   Unexpected '" ++ unpack (closeOf s) ++ "', expected '" ++ unpack (closeOf s') ++ "'\n   " ++ "(to close '" ++ unpack (openOf s') ++ "' from line " ++ show l' ++ ")"
+    ClosedWithoutOpening s l -> "Line " ++ show l ++ ":\n   Unexpected '" ++ unpack (closeOf s) ++ "', closed without opening"
+    Unknown s                -> "A parse error occured: " ++ s
+
+between :: Char -> Char -> Parser ByteString
+between o c = char o *> takeTill (== c) <* char c
+{-# INLINE between #-}
+
+word :: Parser ByteString
+word = takeWhile isLetter
+{-# INLINE word #-}
+
+isEndOfLine :: Char -> Bool
+isEndOfLine c = c == '\n' || c == '\r'
+{-# INLINE isEndOfLine #-}
+
+-- skipTill :: (Char -> Bool) -> Parser ()
+-- skipTill p = skipWhile (not . p)
+-- {-# INLINE skipTill #-}
+
+-- takeWith :: ByteString -> Parser String
+-- takeWith s  =  manyTill anyChar (string s)
+           -- <|> throw (EndOfVerbatim (ByteString.unpack s))
+
+-- skipWith :: State -> ByteString -> Parser ()
+-- skipWith State{line} s  =  (void $ manyTill anyChar (string s))
+           -- <|> (throw $ EndOfFile line (ByteString.unpack s))
+
+-- | Controleer of de string begint met een procent teken. In dat geval gooien
+-- we alle tekens weg tot het einde van de regel, en geven de rest van de string
+-- terug.
+comment :: Parser ByteString
+comment = char '%' *> takeTill isEndOfLine -- Does not consume end of line character!
+
+-- | Controleer of we in een woordelijke omgeving zitten. Alles tussen twee
+-- apenstaartjes negeeren we.
+verbatim :: Parser Symbol
+verbatim  =  At                 <$ char '@'
+         <|> string "\\type" *> choice
+               [ Brace          <$ char '{'
+               , Bracke         <$ char '['
+               , Paren          <$ char '('
+               , Angle          <$ char '<' ]
+         <|> StartStop "typing" <$ string "\\starttyping"
+{-
+verbatim :: Parser [Char]
+verbatim  =  char '@' *> takeWith "@"
+         <|> string "\\type" *> choice
+             [ char '{' *> takeWith "}"
+             , char '[' *> takeWith "]"
+             , char '(' *> takeWith ")"
+             , char '<' *> takeWith ">" ]
+         <|> string "\\starttyping" *> takeWith "\\stoptyping"
+-}
+
+-- | Controleer of de string begint met een geescaped procent teken, dollar
+-- teken of apenstaartje.
+-- We willen immers niet dat we over de backslash heen lezen, en vervolgens de
+-- procent of het dollar teken aanzien voor commentaar dan wel wiskunde...
+escaped :: Parser ByteString
+escaped = choice $ map string ["\\\\", "\\%", "\\$", "\\@"]
+
+-- | Controleer of de string begint met een reeks van tekens dat we als haakje
+-- kunnen gebruiken voor \left of \right. (Doen we op een creatieve manier
+-- waarbij we optimaal gebruik maken van de luiheid van Haskell!)
+delimiter :: Parser ByteString
+delimiter = choice $ map string
+  [ ".", "(", ")", "[", "]", "<", ">", "|", "/"
+  , "\\{", "\\}", "\\|"
+  , "\\lgroup", "\\rgroup", "\\lbrace", "\\rbrace", "\\langle", "\\rangle"
+  , "\\vert", "\\lvert", "\\rvert", "\\Vert", "\\lVert", "\\rVert"
+  , "\\backslash", "\\lfloor", "\\rfloor", "\\lceil", "\\rceil"
+  , "\\uparrow", "\\Uparrow", "\\downarrow", "\\Downarrow", "\\updownarrow", "\\Updownarrow"
+  , "\\llcorner", "\\lrcorner", "\\ulcorner", "\\urconrner"
+  , "\\lmoustache", "\\rmoustache" ]
+
+math :: Parser Symbol
+math = Dollar <$ char '$'
+
+-- | Controleer of de string begint met een openingshaakje, een start- of
+-- begin-commando of met wiskunde. Als dat zo is, strippen we het deze tekst van
+-- de string en geven we `gewoon' een paar terug met daarin het sluithaakje of
+-- stop-commando en de rest van de string. Als we niets vinden, geven we `niets'
+-- terug!
+--
+-- Het dollar teken is een vreemde eend in de bijt. We kunnen namelijk niet
+-- controleren of dit de openings- of sluitings-dollar is. We houden in een
+-- extra argument @m@ bij of we in wiskundemodus zitten of niet. Dit helpt ons
+-- bij het onderscheid.
+opening :: Parser Symbol
+opening  =  Brace     <$  char '{'
+        <|> Bracket   <$  char '['
+        <|> Paren     <$  char '('
+        <|> Delimiter <$  (string "\\left"   *> delimiter)
+        <|> StartStop <$> (string "\\start"  *> word)
+        <|> BeginEnd  <$> (string "\\begin{" *> word <* char '}')
+
+-- | Analoog aan @stripOpening@, maar dan voor sluithaakjes en stop-commando's.
+closing :: Parser Symbol
+closing  =  Brace     <$  char '}'
+        <|> Bracket   <$  char ']'
+        <|> Paren     <$  char ')'
+        <|> Delimiter <$  (string "\\right" *> delimiter)
+        <|> StartStop <$> (string "\\stop"  *> word)
+        <|> BeginEnd  <$> (string "\\end{"  *> word <* char '}')
+
+-- | Recursief algoritme om te controleren of de string gebalanceerd is
+-- wat betreft haakjes en start/stop-paren.
+--
+-- Een korte omschrijving per regel:
+-- * Een lege string met een lege stapel is natuurlijk gebalanceerd.
+-- * Een lege string met een niet lege stapel gaat helemaal mis.
+-- * In andere gevallen:
+--   * We houden het regelnummer bij en hogen het op als we een regeleinde
+--     tegenkomen.
+--   * We controleren of we met een \% of \$ te maken hebben, dat gooien we weg
+--     zodat we verderop niet in de problemen komen.
+--   * Commentaar gooien we weg.
+--   * Wanneer we een openingshaakje tegenkomen zetten we het huidige
+--     regelnummer en het sluithaakje op de stapel.
+--   * Wanneer we een sluithaakje tegenkomen controleren we of dit op de stapel
+--     stond.
+--   * Als de string niet begint met een haakje of start/stop-paar, dan gaan we
+--     door met de rest van de tekst.
+--
+-- Het type van deze functie vraagt eigenlijk om een Writer Monad, maar omdat
+-- dit maar een script is, gaan we het ons niet te ingewikkeld maken...
+balanced :: Parser Bool
+balanced = go mkState where
+    go st  =  (endOfInput >>  end st)
+          <|> (endOfLine  >>  increase st >>= go)
+          <|> (escaped    >>  go st)
+          <|> (comment    >>  go st)
+          -- <|> (verbatim   >>  go st)
+          <|> (verbatim   >>= skip st >>= go)
+          <|> (math       >>= decide st >>= go)
+          <|> (opening    >>= push st >>= go)
+          <|> (closing    >>= pop st >>= go)
+          <|> (anyChar    >>  go st)
+
+end :: Monad m => State -> m Bool
+end State{stack=(line,open):_} = throw $ EndOfFile open line
+end _                          = return True
+
+increase :: Monad m => State -> m State
+increase state@State{line} = return state{line = line + 1}
+
+decide :: Monad m => State -> Symbol -> m State
+decide state@State{mode} symbol = case mode of
+  Math   -> pop state{mode = Normal} symbol
+  Normal -> push state{mode = Math} symbol
+
+push :: Monad m => State -> Symbol -> m State
+push state@State{stack,line} open = return state{stack = (line, open) : stack}
+
+pop :: Monad m => State -> Symbol -> m State
+pop state@State{stack,line} close = case stack of
+  []                   -> throw $ ClosedWithoutOpening close line
+  (line',open):rest
+    | close == open    -> return state{stack = rest}
+    | otherwise        -> throw $ DoesNotMatch close line open line'
+
+skip :: State -> Symbol -> Parser State
+skip state@State{line} symbol =     anyChar `manyTill` string (closeOf symbol)
+                                <|> throw (EndOfFile symbol line)
+                                >>  return state
+
+-- | Lees de tekst in uit een bestand, controleer of deze gebalanceerd is en
+-- geef het resultaat door.
+run :: FilePath -> IO Bool
+run f = do
+  putAct f
+  s <- ByteString.readFile f
+  case parseOnly balanced s of
+    Right bool -> return bool
+    Left  mesg -> throw $ Unknown mesg
+  `catch` \e -> do
+    putErr $ show (e :: BalancingError)
+    return False
+
+-- | Hoofdfunctie waarbij we de commandoregel argumenten inlezen en controleren
+-- of die er überhaupt wel zijn. Vervolgens passen we @run@ toe op elk argument
+-- en geven nog een conclusie of het count fouten.
+main :: IO a
+main = do
+  as <- getArgs
+  when (null as) $ do
+    putWrn "Without arguments I can't do anything!"
+    exitFailure
+
+  rs <- mapM run as
+  let n = length . filter (== False) $ rs
+  case n of
+    0 -> putInf "Everything seems to be all right!"
+    1 -> putInf "Oops, found 1 error!"
+    _ -> putInf $ "Oh help, found " ++ show n ++ " errors!"
+  exitSuccess
+
+-- vim: nowrap
